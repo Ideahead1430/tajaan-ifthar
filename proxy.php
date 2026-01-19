7a3c58a31af3e6e1154cd28e5a6dac32504dfc5e
Ideahead1430 <142952643+Ideahead1430@users.noreply.github.com>
2026-01-19 13:39:43 +0800

    Create proxy.php

---
+++ proxy.php
@@ -0,0 +1,21 @@
+<?php
+// URL Google Apps Script anda
+ $appsScriptUrl = 'https://script.google.com/macros/s/AKfycbzWHnWnT1WDqvLr5_Xj_flcRBOpB65urd7vHmztEA87W6IH0w02E9rOhlbr8UdsG3qE/exec';
+
+// Dapatkan parameter dari panggilan JavaScript
+ $queryString = $_SERVER['QUERY_STRING'];
+
+// Buat URL lengkap
+ $fullUrl = $appsScriptUrl . '?' . $queryString;
+
+// Dapatkan kandungan dari Apps Script
+ $response = file_get_contents($fullUrl);
+
+// Hantar kembali ke JavaScript dengan header yang betul
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+echo $response;
+?>
